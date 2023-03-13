6b3749bbfd713c75ab42956b6550c8b6a113ab4d
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2023-03-13 15:33:52 +1000

    endpoints

---
+++ src/WebApi/Features/TodoItemEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.TodoItems.Commands.CreateTodoItem;
+using Application.Features.TodoItems.Queries.GetAllTodoItems;
+using MediatR;
+
+namespace WebApi.Features;
+
+public static class TodoItemEndpoints
+{
+    public static void MapTodoItemEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("todoitems");
+        
+        group.MapGet("/", (ISender sender, CancellationToken ct) => sender.Send(new GetAllTodoItemsQuery(), ct));
+        
+        group.MapPost("/", (ISender sender, CreateTodoItemCommand command, CancellationToken ct) => sender.Send(command, ct));
+    }
+}
