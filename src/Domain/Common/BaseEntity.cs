70ea0b4e96adb6bb2201a60d14936b75583f19a5
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-14 17:09:00 +1100

    🐛 Fix auditable entities (#31)
    * auditable entity (#30)
    
    * Added missing overrides
    
    * Removed uneeded code
    
    ---------
    
    Co-authored-by: LukeParkerDev <10430890+Hona@users.noreply.github.com>

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -1,10 +1,6 @@
 ﻿namespace Domain.Common;
 
-public class BaseEntity<TId>
+public abstract class BaseEntity<TId> : AuditableEntity
 {
     public TId Id { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public string? CreatedBy { get; set; } // TODO: String as userId?
-    public DateTimeOffset? UpdatedAt { get; set; }
-    public string? UpdatedBy { get; set; } // TODO: String as userId?
 }
