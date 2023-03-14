32c135b04a3a42a66b43bcce73451027e65d3e1c
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2023-03-14 15:55:08 +1000

    auditable entity

---
+++ src/Domain/Common/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public DateTimeOffset CreatedAt { get; set; }
+    public string? CreatedBy { get; set; } // TODO: String as userId?
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; } // TODO: String as userId?
+}
