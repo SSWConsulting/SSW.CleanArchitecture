d17e381b71048db85af18fc6546dbfa3ea42b89f
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-08-09 10:58:04 +1000

    Moved specs to the domain

---
+++ src/Domain/TodoItems/AllTodoItemSpec.cs
@@ -0,0 +1,8 @@
+using Ardalis.Specification;
+using SSW.CleanArchitecture.Domain.TodoItems;
+
+namespace SSW.CleanArchitecture.Application.Features.TodoItems.Specifications;
+
+public sealed class AllTodoItemSpec : Specification<TodoItem>
+{
+}
---
+++ src/Domain/TodoItems/TodoItemByIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using SSW.CleanArchitecture.Domain.TodoItems;
+
+namespace SSW.CleanArchitecture.Application.Features.TodoItems.Specifications;
+
+public sealed class TodoItemByIdSpec : SingleResultSpecification<TodoItem>
+{
+    public TodoItemByIdSpec(TodoItemId todoItemId)
+    {
+        Query.Where(t => t.Id == todoItemId);
+    }
+}
