0286b06008bf92ced7d74593336ab36e34df70bc
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-08-10 20:03:52 +1000

    Added additional HttpStatus codes to endpoint metadata

--- src/WebApi/Extensions/EndpointRouteBuilderExt.cs
+++ src/WebApi/Extensions/EndpointRouteBuilderExt.cs
@@ -2,21 +2,43 @@
 
 public static class EndpointRouteBuilderExt
 {
-    public static RouteHandlerBuilder ProducesGet<T>(this RouteHandlerBuilder builder) => builder
+    /// <summary>
+    /// Used for GET endpoints that return a single entity.
+    /// </summary>
+    public static RouteHandlerBuilder ProducesGetSingle<T>(this RouteHandlerBuilder builder) => builder
+        .Produces<T>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError);
+
+    /// <summary>
+    /// Used for GET endpoints that return a list of entities.
+    /// </summary>
+    public static RouteHandlerBuilder ProducesGetList<T>(this RouteHandlerBuilder builder) => builder
         .Produces<T>(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status500InternalServerError);
 
+    /// <summary>
+    /// Used for POST endpoints that creates a single item.
+    /// </summary>
     public static RouteHandlerBuilder ProducesPost(this RouteHandlerBuilder builder) => builder
         .Produces(StatusCodes.Status201Created)
         .ProducesValidationProblem()
         .ProducesProblem(StatusCodes.Status500InternalServerError);
 
+    /// <summary>
+    /// Used for PUT endpoints that updates a single item.
+    /// </summary>
     public static RouteHandlerBuilder ProducesPut(this RouteHandlerBuilder builder) => builder
         .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound)
         .ProducesValidationProblem()
         .ProducesProblem(StatusCodes.Status500InternalServerError);
 
+    /// <summary>
+    /// Used for DELETE endpoints that deletes a single item.
+    /// </summary>
     public static RouteHandlerBuilder ProducesDelete(this RouteHandlerBuilder builder) => builder
         .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound)
         .ProducesProblem(StatusCodes.Status500InternalServerError);
 }
