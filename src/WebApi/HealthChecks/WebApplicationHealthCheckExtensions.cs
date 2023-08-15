17dd2d8955089856fea024a7521bcaee43345aed
William Liebenberg <awliebenberg@outlook.com>
2023-08-15 11:33:23 +1000

    Add new EF Core DbContext Health Checker - with proper error reportin… (#162)
    * Add new EF Core DbContext Health Checker - with proper error reporting - compare with the latest ms-version
    
    * wrap up the health checks into easy to use extension methods
    
    * - remove user secrets
    - add some specific constructors for DbHealthCheckResult

---
+++ src/WebApi/HealthChecks/WebApplicationHealthCheckExtensions.cs
@@ -0,0 +1,22 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+public static class WebApplicationHealthCheckExtensions
+{
+    public static WebApplication UseHealthChecks(this WebApplication app)
+    {
+        // Basic Healthy/Degraded/Unhealthy result
+        app.UseHealthChecks("/health");
+
+        // Detailed Report about each check
+        // TODO: Because of the detailed information, this endpoint should be secured behind
+        // an Authorization Policy (.RequireAuthorization()), or a specific secured port through firewall rules
+        app.UseHealthChecks("/health-report", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        return app;
+    }
+}
