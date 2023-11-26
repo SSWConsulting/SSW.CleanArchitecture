c898576127b0b35f185405941d9afd19dd21f6b0
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-11-26 13:56:24 +1000

    Fixed up test

--- tests/WebApi.IntegrationTests/Endpoints/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandTests.cs
+++ tests/WebApi.IntegrationTests/Endpoints/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandTests.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SSW.CleanArchitecture.Application.Common.Exceptions;
 using SSW.CleanArchitecture.Application.Features.TodoItems.Commands.CreateTodoItem;
 using SSW.CleanArchitecture.Domain.TodoItems;
+using System.Net;
 using System.Net.Http.Json;
 using WebApi.IntegrationTests.Common.Fixtures;
 
@@ -17,13 +19,16 @@ public class CreateTodoItemCommandTests(TestingDatabaseFixture fixture, ITestOut
         var cmd = new CreateTodoItemCommand("Shopping");
         var client = GetAnonymousClient();
         var createTodoItem = async () => await client.PostAsJsonAsync("/todoitems", cmd);
+        await createTodoItem();
 
-        // Act & Assert
-        await FluentActions.Invoking(async () =>
-        {
-            await createTodoItem();
-            await createTodoItem();
-        }).Should().ThrowAsync<ValidationException>();
+        // Act
+        var result = await createTodoItem();
+        var validation = await result.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        validation.Should().NotBeNull();
+        validation!.Errors.Should().HaveCount(1);
     }
 
     [Fact]
