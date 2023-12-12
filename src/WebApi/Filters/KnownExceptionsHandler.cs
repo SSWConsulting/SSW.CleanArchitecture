263b77e52a22ef1a2fa83287d5f82e0bcbaeaedb
Chris <chris@ssw.com.au>
2023-12-12 11:46:56 +1100

    Apply codeQL suggestion

--- src/WebApi/Filters/KnownExceptionsHandler.cs
+++ src/WebApi/Filters/KnownExceptionsHandler.cs
@@ -14,7 +14,7 @@ public class KnownExceptionsHandler : IExceptionHandler
     public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext,
         Exception exception,
-        CancellationToken ct)
+        CancellationToken cancellationToken)
     {
         Type type = exception.GetType();
 
