b21028f6537d5c527d4dfa2c7e93729a537e2f7d
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-04-14 20:36:35 +1000

    Capture properties when event is raised

--- src/Domain/Heroes/StrengthUpdatedEvent.cs
+++ src/Domain/Heroes/StrengthUpdatedEvent.cs
@@ -1,5 +1,20 @@
-﻿using SSW.CleanArchitecture.Domain.Common.Base;
+﻿using Ardalis.GuardClauses;
+using SSW.CleanArchitecture.Domain.Common.Base;
 
 namespace SSW.CleanArchitecture.Domain.Heroes;
 
-public record StrengthUpdatedEvent(Hero Hero) : DomainEvent;
+public record StrengthUpdatedEvent : DomainEvent
+{
+    public HeroId Id { get; }
+    public string Name { get; }
+    public int Strength { get; }
+
+    public StrengthUpdatedEvent(Hero hero)
+    {
+        Guard.Against.Null(hero);
+
+        Id = hero.Id;
+        Name = hero.Name;
+        Strength = hero.Strength;
+    }
+}
