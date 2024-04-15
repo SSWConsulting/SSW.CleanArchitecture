208bd879a09d2b0bb005174e69134022137fa339
Chris <chris@ssw.com.au>
2024-04-15 16:00:34 +1000

    Added Id comparison tests

--- tests/Domain.UnitTests/Heroes/HeroTests.cs
+++ tests/Domain.UnitTests/Heroes/HeroTests.cs
@@ -4,6 +4,26 @@ namespace SSW.CleanArchitecture.Domain.UnitTests.Heroes;
 
 public class HeroTests
 {
+    [Theory]
+    [InlineData("c8ad9974-ca93-44a5-9215-2f4d9e866c7a", "cc3431a8-4a31-4f76-af64-e8198279d7a4", false)]
+    [InlineData("c8ad9974-ca93-44a5-9215-2f4d9e866c7a", "c8ad9974-ca93-44a5-9215-2f4d9e866c7a", true)]
+    public void HeroId_ShouldBeComparable(string stringGuid1, string stringGuid2, bool isEqual)
+    {
+        // Arrange
+        Guid guid1 = Guid.Parse(stringGuid1);
+        Guid guid2 = Guid.Parse(stringGuid2);
+        HeroId id1 = new(guid1);
+        HeroId id2 = new(guid2);
+
+        // Act
+        var areEqual = id1 == id2;
+
+        // Assert
+        areEqual.Should().Be(isEqual);
+        id1.Value.Should().Be(guid1);
+        id2.Value.Should().Be(guid2);
+    }
+    
     [Fact]
     public void Create_WithValidNameAndAlias_ShouldSucceed()
     {
@@ -97,7 +117,7 @@ public class HeroTests
                 e.PowerLevel.Should().Be(10);
                 e.Id.Should().Be(hero.Id);
                 e.Name.Should().Be(hero.Name);
-            });
+            }).Invoke();
         hero.Powers.Should().ContainSingle("Super-strength");
     }
 
