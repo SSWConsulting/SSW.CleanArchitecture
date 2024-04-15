56c2585716654bdd1c74e1386e68ab26246123c4
Chris <chris@ssw.com.au>
2024-04-15 15:19:48 +1000

    Add migration and add ADR

--- src/Infrastructure/Persistence/Migrations/20240415050433_Remove_StringLength_Constraints.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20240415050433_Remove_StringLength_Constraints.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSW.CleanArchitecture.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using SSW.CleanArchitecture.Infrastructure.Persistence;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240415050433_Remove_StringLength_Constraints")]
+    partial class Remove_StringLength_Constraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
