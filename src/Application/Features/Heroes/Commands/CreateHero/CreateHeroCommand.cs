4875b90ffe2e5ac4fdecf7a0838ce7133bfda1fc
Chris <chris@ssw.com.au>
2024-04-15 17:38:37 +1000

    CreateHeroCommand: remove the need for Team

--- src/Application/Features/Heroes/Commands/CreateHero/CreateHeroCommand.cs
+++ src/Application/Features/Heroes/Commands/CreateHero/CreateHeroCommand.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SSW.CleanArchitecture.Application.Common.Interfaces;
+﻿using SSW.CleanArchitecture.Application.Common.Interfaces;
 using SSW.CleanArchitecture.Domain.Heroes;
-using SSW.CleanArchitecture.Domain.Teams;
 
 namespace SSW.CleanArchitecture.Application.Features.Heroes.Commands.CreateHero;
 
 public sealed record CreateHeroCommand(
     string Name,
     string Alias,
-    Guid TeamId,
     IEnumerable<CreateHeroPowerDto> Powers) : IRequest<Guid>;
 
 // ReSharper disable once UnusedType.Global
@@ -22,14 +19,6 @@ public sealed class CreateHeroCommandHandler(IApplicationDbContext dbContext)
         {
             hero.AddPower(new Power(heroPowerModel.Name, heroPowerModel.PowerLevel));
         }
-
-        Team? team = await dbContext.Teams.FirstOrDefaultAsync(t => t.Id == new TeamId(request.TeamId), cancellationToken);
-        if (team is null)
-        {
-            throw new ArgumentException("Invalid team id", nameof(request.TeamId));
-        }
-        
-        team.AddHero(hero);
 
         await dbContext.Heroes.AddAsync(hero, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
--- tests/WebApi.IntegrationTests/Endpoints/Heroes/Commands/CreateHero/CreateHeroCommandTests.cs
+++ tests/WebApi.IntegrationTests/Endpoints/Heroes/Commands/CreateHero/CreateHeroCommandTests.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using SSW.CleanArchitecture.Application.Features.Heroes.Commands.CreateHero;
-using SSW.CleanArchitecture.Domain.Teams;
 using System.Net;
 using System.Net.Http.Json;
 using WebApi.IntegrationTests.Common.Fixtures;
@@ -8,21 +7,8 @@ using WebApi.IntegrationTests.Common.Fixtures;
 namespace WebApi.IntegrationTests.Endpoints.Heroes.Commands.CreateHero;
 
 public class CreateHeroCommandTests(TestingDatabaseFixture fixture, ITestOutputHelper output)
-    : IntegrationTestBase(fixture, output), IAsyncLifetime
+    : IntegrationTestBase(fixture, output)
 {
-    private TeamId _teamId;
-    
-    public new async Task InitializeAsync()
-    {
-        await base.InitializeAsync();
-
-        var team = Team.Create("TeamSuper");
-        Context.Teams.Add(team);
-        await Context.SaveChangesAsync();
-        
-        _teamId = team.Id;
-    }
-    
     [Fact]
     public async Task Command_ShouldCreateHero()
     {
@@ -35,7 +21,6 @@ public class CreateHeroCommandTests(TestingDatabaseFixture fixture, ITestOutputH
         var cmd = new CreateHeroCommand(
             "Clark Kent", 
             "Superman",
-            _teamId.Value,
             powers.Select(p => new CreateHeroPowerDto { Name = p.Name, PowerLevel = p.PowerLevel }));
         var client = GetAnonymousClient();
 
