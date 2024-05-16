3a7fecb6b223872aebbf3e2ad7dec9acdb27a9be
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-05-16 22:27:51 +1000

    Heroes domain now has seed data

--- src/Infrastructure/Persistence/ApplicationDbContextInitializer.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextInitializer.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using SSW.CleanArchitecture.Domain.TodoItems;
 using Bogus;
+using SSW.CleanArchitecture.Domain.Heroes;
+using SSW.CleanArchitecture.Domain.Teams;
 
 namespace SSW.CleanArchitecture.Infrastructure.Persistence;
 
@@ -9,7 +11,58 @@ public class ApplicationDbContextInitializer(
     ILogger<ApplicationDbContextInitializer> logger,
     ApplicationDbContext dbContext)
 {
-    private const int NumTodoItems = 20;
+    private readonly string[] _superHeroNames =
+    [
+        "Superman",
+        "Batman",
+        "Wonder Woman",
+        "Flash", "Aquaman",
+        "Cyborg",
+        "Green Lantern",
+        "Shazam",
+        "Captain Marvel",
+        "Cyclops",
+        "Wolverine",
+        "Storm"
+    ];
+
+    private readonly string[] _superPowers =
+    [
+        "Strength",
+        "Flight",
+        "Invulnerability",
+        "Speed",
+        "Heat Vision",
+        "X-Ray Vision",
+        "Hearing",
+        "Healing Factor",
+        "Agility",
+        "Stamina",
+        "Breath",
+        "Weapons",
+        "Intelligence"
+    ];
+
+    private readonly string[] _teamNames =
+    [
+        "Marvel",
+        "Avengers",
+        "DC",
+        "Justice League",
+        "X-Men"
+    ];
+
+    private readonly string[] _missionNames =
+    [
+        "Save the world",
+        "Rescue the hostages",
+        "Defeat the villain",
+        "Stop the bomb",
+        "Protect the city"
+    ];
+
+    private const int NumHeroes = 20;
+    private const int NumTeams = 5;
 
     public async Task InitializeAsync()
     {
@@ -31,21 +84,68 @@ public class ApplicationDbContextInitializer(
     {
         try
         {
-            if (dbContext.TodoItems.Any())
-                return;
-
-            var faker = new Faker<TodoItem>()
-                .CustomInstantiator(f => TodoItem.Create(
-                    f.Lorem.Sentence(3), f.Lorem.Sentence(10), f.Random.Enum<PriorityLevel>(), f.Date.Future(1, DateTime.UtcNow)));
-
-            var todoItems = faker.Generate(NumTodoItems);
-            await dbContext.TodoItems.AddRangeAsync(todoItems);
-            await dbContext.SaveChangesAsync();
+            var heroes = await SeedHeroes();
+            await SeedTeams(heroes);
         }
         catch (Exception e)
         {
             logger.LogError(e, "An error occurred while seeding the database");
             throw;
         }
+    }
+
+
+    private async Task<List<Hero>> SeedHeroes()
+    {
+        if (dbContext.Heroes.Any())
+            return [];
+
+        var faker = new Faker<Hero>()
+            .CustomInstantiator(f =>
+            {
+                var name = f.PickRandom(_superHeroNames);
+                var hero = Hero.Create(name, name.Substring(0, 2));
+                var powers = f.PickRandom(_superPowers, f.Random.Number(1, 3))
+                    .Select(p => new Power(p, f.Random.Number(1, 10)));
+                hero.UpdatePowers(powers);
+                return hero;
+            });
+
+        var heroes = faker.Generate(NumHeroes);
+        await dbContext.Heroes.AddRangeAsync(heroes);
+        await dbContext.SaveChangesAsync();
+
+        return heroes;
+    }
+
+    private async Task SeedTeams(List<Hero> heroes)
+    {
+        if (dbContext.Teams.Any())
+            return;
+
+        var faker = new Faker<Team>()
+            .CustomInstantiator(f =>
+            {
+                var name = f.PickRandom(_teamNames);
+                var team = Team.Create(name);
+                var heroesToAdd = f.PickRandom(heroes, f.Random.Number(2, 5));
+
+                foreach (var hero in heroesToAdd)
+                    team.AddHero(hero);
+
+                var sendOnMission = f.Lorem.Random.Bool();
+
+                if (sendOnMission)
+                {
+                    var missionName = f.PickRandom(_missionNames);
+                    team.ExecuteMission(missionName);
+                }
+
+                return team;
+            });
+
+        var teams = faker.Generate(NumTeams);
+        await dbContext.Teams.AddRangeAsync(teams);
+        await dbContext.SaveChangesAsync();
     }
 }
