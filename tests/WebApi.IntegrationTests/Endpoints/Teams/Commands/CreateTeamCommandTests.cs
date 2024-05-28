29a1acf6f30b7f1b9bd298a9c72627b9b6adbe96
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-05-28 15:39:02 +1000

    Added create team test

---
+++ tests/WebApi.IntegrationTests/Endpoints/Teams/Commands/CreateTeamCommandTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using SSW.CleanArchitecture.Application.Features.Teams.Commands.CreateTeam;
+using System.Net;
+using System.Net.Http.Json;
+using WebApi.IntegrationTests.Common.Fixtures;
+
+namespace WebApi.IntegrationTests.Endpoints.Teams.Commands;
+
+public class CreateTeamCommandTests(TestingDatabaseFixture fixture, ITestOutputHelper output)
+    : IntegrationTestBase(fixture, output)
+{
+    [Fact]
+    public async Task Command_ShouldCreateTeam()
+    {
+        // Arrange
+        var cmd = new CreateTeamCommand("Clark Kent");
+        var client = GetAnonymousClient();
+
+        // Act
+        var result = await client.PostAsJsonAsync("/api/teams", cmd);
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+        var item = await Context.Teams.AsNoTracking().FirstAsync();
+
+        item.Should().NotBeNull();
+        item.Name.Should().Be(cmd.Name);
+        item.CreatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+}
