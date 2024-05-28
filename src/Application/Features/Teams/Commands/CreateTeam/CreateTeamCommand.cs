b8b22ab3e1627b44dd9e4577de43e864aca8294b
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-05-28 15:22:26 +1000

    Add command and validator

---
+++ src/Application/Features/Teams/Commands/CreateTeam/CreateTeamCommand.cs
@@ -0,0 +1,28 @@
+﻿using SSW.CleanArchitecture.Application.Common.Interfaces;
+using SSW.CleanArchitecture.Domain.Teams;
+
+namespace SSW.CleanArchitecture.Application.Features.Teams.Commands.CreateTeam;
+
+public sealed record CreateTeamCommand(string Name) : IRequest;
+
+// ReSharper disable once UnusedType.Global
+public sealed class CreateTeamCommandHandler(IApplicationDbContext dbContext)
+    : IRequestHandler<CreateTeamCommand>
+{
+    public async Task Handle(CreateTeamCommand request, CancellationToken cancellationToken)
+    {
+        var team = Team.Create(request.Name);
+
+        await dbContext.Teams.AddAsync(team, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
+
+public class CreateTeamCommandValidator : AbstractValidator<CreateTeamCommand>
+{
+    public CreateTeamCommandValidator()
+    {
+        RuleFor(v => v.Name)
+            .NotEmpty();
+    }
+}
