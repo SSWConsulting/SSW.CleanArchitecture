1809e483d5e296470cf1d7c80e8bc6da28a354f6
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-06-07 15:42:06 +1000

    Move migrations to the correct folder

--- src/Infrastructure/Persistence/Migrations/20240529121636_Remove_Todos.cs
+++ src/Infrastructure/Persistence/Migrations/20240529121636_Remove_Todos.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace SSW.CleanArchitecture.Infrastructure.Migrations
+namespace Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
     public partial class Remove_Todos : Migration
