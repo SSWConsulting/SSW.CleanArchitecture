54f92be4e693e75f1e27913a712d547365a34a25
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-11-18 16:33:16 +1000

    Update item templates

--- templates/command/Commands/CommandName/CommandNameCommand.cs
+++ templates/command/Commands/CommandName/CommandNameCommand.cs
@@ -4,19 +4,21 @@ namespace SSW.CleanArchitecture.Application.UseCases.EntityNames.Commands.Comman
 
 public record CommandNameCommand() : IRequest<ErrorOr<Success>>;
 
-public class CommandNameCommandHandler : IRequestHandler<CommandNameCommand, ErrorOr<Success>>
+internal sealed class CommandNameCommandHandler(IApplicationDbContext dbContext)
+    : IRequestHandler<CommandNameCommand, ErrorOr<Success>>
 {
-    private readonly IApplicationDbContext _dbContext;
-
-    public CommandNameCommandHandler(IApplicationDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
     public async Task<ErrorOr<Success>> Handle(CommandNameCommand request, CancellationToken cancellationToken)
     {
         // TODO: Add your business logic and persistence here
 
         throw new NotImplementedException();
+    }
+}
+
+public class CommandNameCommandValidator : AbstractValidator<CommandNameCommand>
+{
+    public CommandNameCommandValidator()
+    {
+        // TODO: Add your validation rules here.  For example: RuleFor(p => p.Foo).NotEmpty()
     }
 }
--- templates/command/Commands/CommandName/CommandNameCommandValidator.cs
+++
@@ -1,9 +0,0 @@
-namespace SSW.CleanArchitecture.Application.UseCases.EntityNames.Commands.CommandName;
-
-public class CommandNameCommandValidator : AbstractValidator<CommandNameCommand>
-{
-    public CommandNameCommandValidator()
-    {
-        // TODO: Add your validation rules here.  For example: RuleFor(p => p.Foo).NotEmpty()
-    }
-}
