ad55134de04ac5819eaf48c10e6605647eb60a4b
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2025-03-27 22:00:52 +1000

    Slight modification to collection fixture

--- tests/WebApi.IntegrationTests/Common/IntegrationTestBase.cs
+++ tests/WebApi.IntegrationTests/Common/IntegrationTestBase.cs
@@ -7,7 +7,7 @@ namespace WebApi.IntegrationTests.Common;
 /// <summary>
 /// Integration tests inherit from this to access helper classes
 /// </summary>
-[Collection(TestingDatabaseFixtureCollection.Name)]
+[Collection<TestingDatabaseFixtureCollection>]
 public abstract class IntegrationTestBase : IAsyncLifetime
 {
     private readonly IServiceScope _scope;
--- tests/WebApi.IntegrationTests/Common/TestingDatabaseFixtureCollection.cs
+++ tests/WebApi.IntegrationTests/Common/TestingDatabaseFixtureCollection.cs
@@ -1,11 +1,11 @@
 namespace WebApi.IntegrationTests.Common;
 
-[CollectionDefinition(Name)]
+[CollectionDefinition]
 public class TestingDatabaseFixtureCollection : ICollectionFixture<TestingDatabaseFixture>
 {
     // This class has no code and is never created. Its purpose is simply
     // to be the place to apply [CollectionDefinition] and all the
     // ICollectionFixture<> interfaces.
 
-    public const string Name = nameof(TestingDatabaseFixtureCollection);
+    // public const string Name = nameof(TestingDatabaseFixtureCollection);
 }
